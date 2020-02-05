71bca784727df169fc3e7b8db79b411b5738569f
Renat Sitrov <renat.sitrov@mail.ru>
2020-02-05 15:00:00 +0500

    no msg

---
+++ app/src/main/java/com/hfad/starbuzz/TopLevelActivity.java
@@ -0,0 +1,31 @@
+package com.hfad.starbuzz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class TopLevelActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_level);
+        //Создать onItemClickListener
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                if (position == 0){
+                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
+        //Добавить слушателя к списковому представлению
+        ListView listView = findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
+    }
+}
