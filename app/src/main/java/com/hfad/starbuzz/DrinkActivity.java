bf15620adeeb643e603fd44f7acdec1134f8c50e
Renat Sitrov <renat.sitrov@mail.ru>
2020-02-13 12:34:52 +0500

    favorite added

--- app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
+++ app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
@@ -1,11 +1,14 @@
 package com.hfad.starbuzz;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,7 +30,7 @@ public class DrinkActivity extends Activity {
             SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase();
 
             Cursor cursor = db.query("DRINK",
-                    new String[] {"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"},
+                    new String[] {"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID", "FAVORITE"},
                     "_id = ?",
                     new String[] {Integer.toString(drinkId)},
                     null, null, null);
@@ -36,6 +39,7 @@ public class DrinkActivity extends Activity {
                 String nameText = cursor.getString(0);
                 String descriptionText = cursor.getString(1);
                 int photoId = cursor.getInt(2);
+                boolean isFavorite = (cursor.getInt(3) == 1);
 
                 //Заполнение названия напитка
                 TextView name = findViewById(R.id.name);
@@ -49,6 +53,9 @@ public class DrinkActivity extends Activity {
                 ImageView photo = findViewById(R.id.photo);
                 photo.setImageResource(photoId);
                 photo.setContentDescription(nameText);
+
+                CheckBox favorite = findViewById(R.id.favorite);
+                favorite.setChecked(isFavorite);
             }
             cursor.close();
             db.close();
@@ -57,5 +64,21 @@ public class DrinkActivity extends Activity {
             toast.show();
         }
 
+    }
+
+    public void onFavoriteClicked(View view){
+        int drinkId = (Integer) getIntent().getExtras().get(EXTRA_DRINKID);
+        CheckBox favorite = findViewById(R.id.favorite);
+        ContentValues drinkValues = new ContentValues();
+        drinkValues.put("FAVORITE", favorite.isChecked());
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try{
+            SQLiteDatabase db = starbuzzDatabaseHelper.getWritableDatabase();
+            db.update("DRINK", drinkValues, "_id=?", new String[] {Integer.toString(drinkId)});
+            db.close();
+        } catch (SQLiteException e){
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
